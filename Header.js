5fd2fa4f5505ea9042b0d1dbfbc3ee46f6d84b45
pawankalayan6899 <99861196+pawankalayan6899@users.noreply.github.com>
2024-08-19 02:46:05 +0530

    Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ padding: '10px', backgroundColor: '#282c34', color: 'white' }}>
+      <h1>Spatial Data Map Viewer</h1>
+    </header>
+  );
+};
+
+export default Header;
